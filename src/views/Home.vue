11b0b6816d607fd630ac1d459d0e83e755610924
“Vishnu <vvichugit@gmail.com>
2018-08-04 23:50:20 -0500

    this is the initail_cum_final commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,88 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>{{ message }}</h2>
+    <input type="text" v-model="message">
+    <h2>{{ message_2 }}</h2>
+    <h2>Length of Task List: {{ numIncompleteTask() }} remaining</h2>
+    <button v-on:click="deleteCompleted_tasks()">Clear_Completed_Tasks</button>
+    <button v-on:click="addTasks()">Add_tasks</button>
+    <h3>Task_id:</h3>  <input type = "text" v-model = "newTasks.id">
+    <h3>Task_name:</h3> <input type="text" v-model = "newTasks.text">
+    <br>
+    <h2>{{ task }}</h2>
+    <ul>
+       <li v-for="task in tasks">
+           {{ task.id }}: <span v-on:click="completeTask(task)" v-bind:class="{strike:task.completion}">{{ task.text }}</span>
+       </li>    
+    </ul>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+<style>
+.strike {
+  text-decoration: line-through;
 }
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Vishnu is a pro in programming",
+      message_2: "VIshnu is a Computer programmer",
+      task: "List of_Tasks",
+      tasks: [
+        { id: 1, text: "Clean the Dishes", completion: false },
+        { id: 2, text: "Iron the Shirts", completion: false },
+        { id: 3, text: "Wash the car", completion: false }
+      ],
+
+      newTasks: { id: "", text: "", completion: false }
+    };
+  },
+  created: function() {},
+  methods: {
+    addTasks: function() {
+      console.log("Computer Programming", this.newTasks);
+      if (this.newTasks.text !== "") {
+        this.tasks.push(this.newTasks);
+        this.newTasks = { id: "", text: "", completion: false };
+      }
+    },
+
+    deleteTask: function(inputTask) {
+      console.log("task is deleted....", inputTask.text);
+      var index = this.tasks.indexOf(inputTask);
+      this.tasks.splice(index, 1);
+    },
+
+    completeTask: function(inputTask) {
+      inputTask.completion = !inputTask.completion;
+    },
+
+    numIncompleteTask: function() {
+      var count = 0;
+      this.tasks.forEach(function(task) {
+        if (!task.completion) {
+          count += 1;
+        }
+      });
+
+      return count;
+    },
+
+    deleteCompleted_tasks: function() {
+      var incompleteTasks = [];
+      this.tasks.forEach(function(task) {
+        if (!task.completion) {
+          incompleteTasks.push(task);
+        }
+      });
+
+      this.tasks = incompleteTasks;
+    }
+  },
+
+  computed: {}
+};
 </script>
